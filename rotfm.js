b01f627d2e7f011662c969ecae2b1e3224f37c38
Erik Itter <erik.itter@gmail.com>
2024-02-05 23:29:26 +0100

    calculations and tools

--- rotfm.js
+++ rotfm.js
@@ -18,3 +18,24 @@ let party = [
 party = party.sort((a, b) => a.localeCompare(b));
 
 const loot = [];
+
+function reportLootForPerson(name, loot) {
+    let s = `<table>`;
+
+    loot.filter(i => i.holder === name).forEach(i => { `<tr><td>${i.name}</td><td>${i.price}</td></tr>` });
+
+    s = s + `</table>`;
+
+    return s;
+}
+
+function reportLoot(party, loot) {
+
+    let content = `<table>`;
+
+    party.forEach(c => { content = content + `<tr>${reportLootForPerson(c.name, loot)}</tr>` });
+
+    content = content + `</table>`;
+
+    ChatMessage.create({ content: content });
+}
